a5312096f8fe7af300c3e19968fb412f56fa7c0a
Mike Starr <imhs@protonmail.com>
2022-01-12 10:21:46 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution here
+function findMatching(arr, name) {
+  return arr.filter((el) => el.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(arr, str) {
+  return arr.filter((i) => {
+    return i.substring(0, 2).includes(str);
+  });
+}
+
+function matchName(arr, name) {
+  return arr.filter((i) => i.name === name);
+}
+
+const drivers = [
+  {
+    name: "Bobby",
+    hometown: "Pittsburgh",
+  },
+  {
+    name: "Sammy",
+    hometown: "New York",
+  },
+  {
+    name: "Sally",
+    hometown: "Cleveland",
+  },
+  {
+    name: "Annette",
+    hometown: "Los Angeles",
+  },
+  {
+    name: "Bobby",
+    hometown: "Tampa Bay",
+  },
+];
+
+console.log(matchName(drivers, "Bobby"));
